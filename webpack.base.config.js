42e0de676447843a7053fc1ffe05446541a91b03
zpzhh <2474538315@qq.com>
2020-09-21 11:25:23 +0800

    重新配置webpack两种不同环境下的配置文件，修改依赖。

---
+++ webpack.base.config.js
@@ -0,0 +1,135 @@
+const path=require("path");
+const webpack=require("webpack");
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const ExtractTextPlugin=require("extract-text-webpack-plugin");
+const UglifyJsPlugin=require("uglifyjs-webpack-plugin");
+const FriendlyErrorsPlugin=require("friendly-errors-webpack-plugin");
+const OptimizeCssAssetsPlugin=require("optimize-css-assets-webpack-plugin");
+const purifyCss=require("purifycss-webpack");
+const CommonsChunksPlugin=require("webpack/lib/optimize/CommonsChunkPlugin");
+const glob=require("glob-all");
+const isProd=process.env.NODE_ENV==="production";
+
+module.exports={
+    devtool:isProd?false:"#cheap-module-source-map",
+    output:{
+      path:path.join(__dirname,"./dist"),
+      filename:"[name][hash].js"
+    },
+    resolve:{
+        alias:{
+
+        }
+    },
+    module:{
+        rules:[
+            {
+                test:/\.vue$/,
+                loader:"vue-loader",
+                options:{
+                   loaders:{
+                       scss:isProd?ExtractTextPlugin.extract({
+                        fallback:"vue-style-loader",
+                        use:["css-loader","sass-loader"],
+                    }):["vue-style-loader","css-loader","sass-loader"],
+                    css:isProd?ExtractTextPlugin.extract({
+                        fallback:"vue-style-loader",
+                        use:["css-loader"],
+                    }):["vue-style-loader","css-loader"]
+                   }
+               }
+            },
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:[{
+                    loader:'babel-loader',
+                    options:{
+                        presets:["env"]
+                    }
+                }]
+    
+            },
+            {
+                test:/\.(png|jpg|gif|svg)$/,
+                loader:"url-loader",
+                options:{
+                    limit:10000,
+                    name:'[name].[ext]?[hash]'
+                }
+            },
+            {
+                test:/\.(wav|mp3|eot|ttf|woff)$/,
+                loader:'file-loader',
+                options:{
+                    name:"fonts/[contenthash].[ext]"
+                }
+            },
+            {
+                test:/\.css$/,
+                use:isProd?ExtractTextPlugin.extract({
+                    use:[{
+                        loader:"css-loader",
+                        options:{
+                            minimize:true,
+                        }
+                    }],
+                    fallback:'vue-style-loader',
+                }):["vue-style-loader","css-loader"]
+            },
+            {
+                test:/\.scss$/,
+                use:isProd?ExtractTextPlugin.extract({
+                     use:[
+                         {
+                             loader:'css-loader',
+                             options:{
+                                 minimize:true,
+                             }
+                         },
+                         "sass-loader",  
+                     ],
+                     fallback:'vue-style-loader'
+                }):["vue-style-loader","css-loader","sass-loader"]
+            }
+        ]
+    },
+    performance:{
+        hints:false,
+    },
+    plugins:isProd?[
+        new HtmlWebpackPlugin({
+            template:path.join(__dirname,"./index.html"),
+            filename:'index.html'
+        }),
+        new UglifyJsPlugin({
+            compress:{
+                warnings:false,
+            }
+        }),
+        new ExtractTextPlugin({
+            filename:"css/styles.css",
+        }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessorOptions:{safe:true,discardComments:{removeAll:true}},
+            canPrint:true
+        }),
+        new purifyCss({
+            paths:glob.sync([
+                path.resolve(__dirname,"./*.html"),
+                path.resolve(__dirname,"./*.js")
+            ])
+        }),
+        new CommonsChunksPlugin({
+            name:["base","jquery","vue"],
+            minChunks:2
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+    ]:[
+        new FriendlyErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            template:path.join(__dirname,"./index.html"),
+            filename:'index.html'
+        })
+    ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,20 @@
 let path=require('path');
-let HtmlWebpackPlugin=require('html-webpack-plugin');
-const {CleanWebpackPlugin}=require("clean-webpack-plugin");
-const ExtractTextPlugin=require("extract-text-webpack-plugin");
-const OptimizeCssAssetsPlugin=require("optimize-css-assets-webpack-plugin");
-const UglifyJsPlugin=require("uglifyjs-webpack-plugin");
-const HardSource=require("hard-source-webpack-plugin");
-const CommonsChunksPlugin=require("webpack/lib/optimize/CommonsChunkPlugin");
-const purifyCss=require("purifycss-webpack");
-const glob=require("glob-all");
-const webpack=require("webpack");
 
-module.exports={
+const {CleanWebpackPlugin}=require("clean-webpack-plugin");
+const HardSource=require("hard-source-webpack-plugin");
+const merge=require("webpack-merge");
+const webpack=require("webpack");
+const base=require("./webpack.base.config");
+
+const isProd=process.env.NODE_ENV=="production";
+
+const config=merge(base,{
     entry:{
         main:path.join(__dirname,'./index.js'),
         jquery:["jquery"],
         vue:["vue"],
-    },
-    output:{
-       path:path.join(__dirname,'./dist'),
-       filename:'[name].js'
-    },
-    externals:{
-      $:'jquery',
-    },
-    devServer:{
+    }, 
+    devServer:isProd?{}:{
         contentBase:false,
         historyApiFallback:false,
         hot:true,
@@ -34,133 +25,20 @@ module.exports={
             poll:1000,
         }
     },
-    module:{
-        rules:[
-            {
-                test:/\.vue$/,
-                loader:"vue-loader",
-               options:{
-                   loaders:{
-                       scss:ExtractTextPlugin.extract({
-                        fallback:"vue-style-loader",
-                        use:["css-loader","sass-loader"],
-                    }),
-                    css:ExtractTextPlugin.extract({
-                        fallback:"vue-style-loader",
-                        use:["css-loader"],
-                    })
-                   }
-               }
-            },
-            {
-                test:/\.css$/,
-                use:ExtractTextPlugin.extract({
-                    fallback:"style-loader",
-                    use:[{
-                        loader:"css-loader",
-                        options:{
-                            minimize:true,
-                        }
-                    }]
-                })
-            },
-            
-        {
-            test:/\.scss$/,
-           use:ExtractTextPlugin.extract({
-               fallback:"style-loader",
-               use:[{
-                   loader:"css-loader",
-                   options:{
-                       minimize:true,
-                   }
-               },"sass-loader"],
+})
+
+if(!isProd){
+   [
+       new HardSource(),
+       new CleanWebpackPlugin({
+          root:path.resolve(__dirname,"./dist"),
            }),
-        },
+       new webpack.HotModuleReplacementPlugin(),
 
-        {
-            test:/\.(png|jpe?g|gif|svg)$/,
-            loader:'url-loader',
-            options:{
-                    limit:1000,
-                    name:'./images/[name].[hash:7].[ext]'
-            }
-        },
-        {
-            test:/\.(wav|mp3|eot|ttf|woff)$/,
-            loader:'file-loader',
-            options:{
-                name:"fonts/[contenthash].[ext]"
-            }
-        },
-        {
-            test:/\.js$/,
-            exclude:/node_modules/,
-            use:[{
-                loader:'babel-loader',
-                options:{
-                    presets:["env"]
-                }
-            }]
 
-        }
-       
-    ]
-    },
-    plugins:[
-        // new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            template:path.join(__dirname,'./index.html'),
-            filename:'./index.html',
-            minimize:true,
-            // minify:{
-            //     removeComments:true,
-            //     collapseWhitespace:true
-            // },
-        }),
-        new CleanWebpackPlugin({
-            root:path.resolve(__dirname,"./dist"),
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-      /* new UglifyJsPlugin({
-            cache:true,
-        }),*/
-        new ExtractTextPlugin("css/styles.css"),
-        // new OptimizeCssAssetsPlugin({
-        //     cssProcessorOptions:{safe:true,discardComments:{removeAll:true}},
-        //     canPrint:true
-        // }),
-        // new UglifyJsPlugin({
-        //     test:/\.js$/,
-        //     uglifyOptions:{
-        //         compress:{
-        //             warning:false,
-        //             drop_console:true,
-        //             collapse_vars:true,
-        //             reduce_vars:true
-        //         },
-        //         output:{
-        //           beautify:false,
-        //           comments:false,
-        //         },
-        //         minimize:true,
-        //         mangle:true,
-        //     }
-        // }),
-        new HardSource(),
-        // new CommonsChunksPlugin({
-        //     name:["base","jquery","vue"],
-        //     minChunks:2
-        // }),
-        // new purifyCss({
-        //     paths:glob.sync([
-        //         path.resolve(__dirname,"./*.html"),
-        //         path.resolve(__dirname,"./*.js")
-        //     ])
-        // }),
-
-        // new webpack.optimize.ModuleConcatenationPlugin(),
-
-        ]
-
+].forEach((plugin)=>{
+    config.plugins.push(plugin);
+   })
 }
+
+module.exports=config;
